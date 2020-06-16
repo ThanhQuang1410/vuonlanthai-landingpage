dbaa8ae67c7a54ad57c7203472b8cc32e371f312
Trần Quang <ttquang1410@gmail.com>
2020-06-16 14:42:24 +0700

    Updates

---
+++ precache-manifest.884e4309e46204a8b88368828fbaa910.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7451a5c3f3d462494344a5e199f750f",
+    "url": "/vuonlanthai-landingpage/index.html"
+  },
+  {
+    "revision": "2172833aada588343413",
+    "url": "/vuonlanthai-landingpage/static/css/2.4fd09901.chunk.css"
+  },
+  {
+    "revision": "1b5ceac0dbb358124523",
+    "url": "/vuonlanthai-landingpage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2172833aada588343413",
+    "url": "/vuonlanthai-landingpage/static/js/2.b4cf8965.chunk.js"
+  },
+  {
+    "revision": "bac08d190ad2c3782685257760a40a06",
+    "url": "/vuonlanthai-landingpage/static/js/2.b4cf8965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5ceac0dbb358124523",
+    "url": "/vuonlanthai-landingpage/static/js/main.2afb081f.chunk.js"
+  },
+  {
+    "revision": "8e0d00dc63b6fb8ae381",
+    "url": "/vuonlanthai-landingpage/static/js/runtime-main.4262adc4.js"
+  },
+  {
+    "revision": "fd5b092f0529bd693d4ef39110e08489",
+    "url": "/vuonlanthai-landingpage/static/media/transparent.fd5b092f.png"
+  }
+]);
